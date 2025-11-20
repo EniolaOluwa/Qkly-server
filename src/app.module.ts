4103363872d44cb1b4f17228b84d6ce58c31a9c5
Adetayo <akinsanyaadetayo@gmail.com>
2025-11-20 10:34:14 +0100

    fix issue with email service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,23 @@
 import { Module } from '@nestjs/common';
-import { APP_GUARD, Reflector } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD, Reflector } from '@nestjs/core';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './core/users/users.module';
-import { BusinessesModule } from './core/businesses/businesses.module';
-import { WalletsModule } from './core/wallets/wallets.module';
-import { ReviewModule } from './core/store/review.module';
-import { ProductModule } from './core/product/product.module';
-import { OrderModule } from './core/order/order.module';
-import { dataSource } from './database';
-import { JwtAuthGuard } from './core/users';
-import { JwtStrategy } from './common/auth/jwt.strategy';
-import { PassportModule } from '@nestjs/passport';
-import { CategoryModule } from './core/category/category.module';
 import { AdminController } from './core/admin/admin.controller';
+import { BusinessesModule } from './core/businesses/businesses.module';
+import { CategoryModule } from './core/category/category.module';
 import { LeadModule } from './core/lead/lead.module';
-import { EmailModule } from './core/email/email.module';
+import { OrderModule } from './core/order/order.module';
 import { PaymentModule } from './core/payment/payment.module';
+import { ProductModule } from './core/product/product.module';
+import { ReviewModule } from './core/store/review.module';
 import { TransactionService } from './core/transaction/transaction.service';
+import { JwtAuthGuard } from './core/users';
+import { UsersModule } from './core/users/users.module';
+import { WalletsModule } from './core/wallets/wallets.module';
+import { dataSource } from './database';
 
 
 @Module({
@@ -33,7 +31,6 @@ import { TransactionService } from './core/transaction/transaction.service';
     UsersModule,
     ProductModule,
     OrderModule,
-    EmailModule,
     BusinessesModule,
     WalletsModule,
     ReviewModule,
