c9613e8e2b13d1974fd5d86a43cb576137565ea8
Adetayo <akinsanyaadetayo@gmail.com>
2025-11-22 00:58:42 +0100

    fix issue with product fetching

--- src/core/payment/payment.module.ts
+++ src/core/payment/payment.module.ts
@@ -2,11 +2,9 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
-import { OrderService } from '../order/order.service';
 import { SharedRepositoryModule } from '../shared/shared-repository.module';
 import { PaymentService } from './payment.service';
 import { PaystackIntegrationService } from './paystack-integration.service';
-import { PaystackPollingService } from './paystack-polling.service';
 import { MonnifyProvider } from './providers/monnify.provider';
 import { PaystackWebhookHandler } from './providers/paystack-webhook.handler';
 import { PaystackProvider } from './providers/paystack.provider';
@@ -23,15 +21,12 @@ import { PaystackProvider } from './providers/paystack.provider';
     PaystackProvider,
     PaystackWebhookHandler,
     PaystackIntegrationService,
-    PaystackPollingService,
-    // Remove OrderService from here
   ],
   exports: [
     PaymentService,
     PaystackProvider,
     PaystackWebhookHandler,
     PaystackIntegrationService,
-    PaystackPollingService
   ],
 })
 export class PaymentModule { }
